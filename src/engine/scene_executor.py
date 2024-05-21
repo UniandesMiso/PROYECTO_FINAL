63a4b458c9e4c6c7ce5ac44f22840776df5f122a
Ivan Rodriguez <ie.rodriguez@uniandes.edu.co>
2024-05-20 20:33:34 -0500

    scene structure

--- src/engine/scene_executor.py
+++ src/engine/scene_executor.py
@@ -1,4 +1,5 @@
 import src.engine.game_engine
+from src.engine.scenes_strategy.menu_scene import MenuScene
 from src.engine.scenes_strategy.play_scene import PlayScene
 from src.engine.scenes_strategy.scene_strategy import SceneStrategy
 
@@ -9,6 +10,7 @@ class SceneExecutor:
         self.game_engine = game_engine
         self.window_cfg = window_cfg
         self.scene_dict = {
+            'MENU': MenuScene(self.game_engine, self.window_cfg)
         }
 
     def scene_executor(self, scene_name: str, **kwargs) -> SceneStrategy:
