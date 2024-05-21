a38d8c0efea8607c61667d07cf520818d65d6a8c
felipe <lf.garciar1@uniandes.edu.co>
2024-05-20 20:33:34 -0500

    scene structure

---
+++ src/engine/scenes_strategy/menu_scene.py
@@ -0,0 +1,20 @@
+from src.ecs.components.c_input_command import CInputCommand
+from src.engine.scenes_strategy.scene_strategy import SceneStrategy
+
+
+class MenuScene(SceneStrategy):
+    def do_create(self):
+        pass
+
+    def do_calculate_time(self, delta_time: float):
+        pass
+
+    def do_update(self, delta_time: float):
+        pass
+
+    def do_action(self, action: CInputCommand):
+        pass
+
+    def do_clean(self):
+        pass
+
